1fa9bd7b49bb055f240d5071173fc71fda2fbc6c
corykim0829 <corykim0829@gmail.com>
2020-04-21 21:03:54 +0900

    feat: 가격 Label들을 담을 PriceLabelsStackView 생성
    Issue: #6

---
+++ ios/SideDishApp/SideDishApp/Views/Product/PriceLabelsStackView.swift
@@ -0,0 +1,16 @@
+//
+//  PriceLabelsStackView.swift
+//  SideDishApp
+//
+//  Created by Cory Kim on 2020/04/21.
+//  Copyright © 2020 corykim0829. All rights reserved.
+//
+
+import UIKit
+
+class PriceLabelsStackView: UIStackView {
+
+    func configurePriceLabels(normalPrice: String?, salePrice: String) {
+        
+    }
+}
