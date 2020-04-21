541c6ef8c7e0afd76bb46443585bcc92b0c2da70
corykim0829 <corykim0829@gmail.com>
2020-04-21 14:51:16 +0900

    feat: tableView 연결 및 임시로 표시 및 rowHeight 지정
    Issue: #1

--- ios/SideDishApp/SideDishApp/ViewControllers/HomeViewController.swift
+++ ios/SideDishApp/SideDishApp/ViewControllers/HomeViewController.swift
@@ -10,9 +10,37 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    private let rowHeight: CGFloat = 130
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        tableView.dataSource = self
+        tableView.delegate = self
     }
 }
 
+extension HomeViewController: UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: ProductCell.identifier, for: indexPath) as! ProductCell
+        return cell
+    }
+}
+
+extension HomeViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return rowHeight
+    }
+}
