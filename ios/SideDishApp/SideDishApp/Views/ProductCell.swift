786401ba0db5a4dd48fef0cbd9425f70b5f1144c
corykim0829 <corykim0829@gmail.com>
2020-04-21 14:35:36 +0900

    feat: Product Cell 생성 및 스토리보드 연결
    identifier 지정
    
    Issue: #1

---
+++ ios/SideDishApp/SideDishApp/Views/ProductCell.swift
@@ -0,0 +1,18 @@
+//
+//  ProductCell.swift
+//  SideDishApp
+//
+//  Created by Cory Kim on 2020/04/21.
+//  Copyright © 2020 corykim0829. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UITableViewCell {
+
+    static let identifier = "product"
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
