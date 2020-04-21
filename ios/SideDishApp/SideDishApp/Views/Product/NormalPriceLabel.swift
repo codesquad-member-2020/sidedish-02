db08a6d59554fbc563acc19826438d1ab22690b3
corykim0829 <corykim0829@gmail.com>
2020-04-21 21:20:19 +0900

    feat: NormalPriceLabel 구현
    AttributedString을 사용하여 strike, 폰트 설정 구현
    
    Issue: #6

---
+++ ios/SideDishApp/SideDishApp/Views/Product/NormalPriceLabel.swift
@@ -0,0 +1,25 @@
+//
+//  NormalPriceLabel.swift
+//  SideDishApp
+//
+//  Created by Cory Kim on 2020/04/21.
+//  Copyright © 2020 corykim0829. All rights reserved.
+//
+
+import UIKit
+
+class NormalPriceLabel: UILabel {
+
+    private let normalPriceColor = UIColor(named: "subtitle-gray")
+    private let fontSize: CGFloat = 14
+    
+    func setTitle(text: String) {
+        attributedText = NSAttributedString(string: text,
+                                            attributes: [
+                                                .foregroundColor: normalPriceColor,
+                                                .font: UIFont.systemFont(ofSize: fontSize),
+                                                .strikethroughStyle: NSUnderlineStyle.single.rawValue,
+                                                .strikethroughColor: normalPriceColor,
+                                            ])
+    }
+}
