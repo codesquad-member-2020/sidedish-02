78e26c8bd733b5a262b8bf7155266bd77fb5bcfd
corykim0829 <corykim0829@gmail.com>
2020-04-24 19:52:48 +0900

    refactor: Model 디렉토리에 커스텀 클래스 파일 생성
    Issue: #23

---
+++ ios/SideDishApp/SideDishApp/Model/ProductList.swift
@@ -0,0 +1,17 @@
+//
+//  ProductList.swift
+//  SideDishApp
+//
+//  Created by Cory Kim on 2020/04/24.
+//  Copyright © 2020 corykim0829. All rights reserved.
+//
+
+import Foundation
+
+struct ProductList: Decodable {
+    let products: [Product]
+    
+    enum CodingKeys: String, CodingKey {
+        case products = "body"
+    }
+}
