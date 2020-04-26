c0caf0029759d8e691edef7295b507de1d425128
David Chung <david215@berkeley.edu>
2020-04-27 00:38:56 +0900

    chore: 프로젝트 생성
    - 프로젝트 생성 및 gradle 의존성 추가

---
+++ be/src/main/java/io/codesquad/group2/banchan/BanchanApplication.java
@@ -0,0 +1,14 @@
+package io.codesquad.group2.banchan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+public class BanchanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BanchanApplication.class, args);
+	}
+
+}
---
+++ be/src/test/java/io/codesquad/group2/banchan/BanchanApplicationTests.java
@@ -0,0 +1,13 @@
+package io.codesquad.group2.banchan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BanchanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
