33802aca7e9a0e81f9b05fd0cadb369556701913
corykim0829 <corykim0829@gmail.com>
2020-04-29 14:46:22 +0900

    feat: SignInViewController 생성 및 기초 UI 작업
    Issue: #5

---
+++ ios/SideDishApp/SideDishApp/ViewControllers/SignInViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SignInViewController.swift
+//  SideDishApp
+//
+//  Created by Cory Kim on 2020/04/29.
+//  Copyright © 2020 corykim0829. All rights reserved.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+}
